3d7af7f90906d30d82ba8345bc946e05af04492f
hauvvph38983 <vivanhau2k4@gmail.com>
2025-02-11 13:56:58 +0700

    Hoàn thiện Enemy_NO

--- Assets/_Game/Scripts/Enemy/ExplodeEnemyMovement.cs
+++ Assets/_Game/Scripts/Enemy/ExplodeEnemyMovement.cs
@@ -1,12 +1,19 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class ExplodeEnemyMovement : EnemyMovement
 {
     public float distanceAttack = 0.5f;
     public float waitMoveSpeed = 0;
+    public float damageRadius = 3f;  
+    public float damageAmount = 20f;
+
+    public LayerMask targetLayer;
+    public GameObject flame;
+    public ParticleSystem ParticleSystem;
 
     private Color color1 = Color.white;
     private Color color2 = Color.red;
@@ -41,6 +48,21 @@ public class ExplodeEnemyMovement : EnemyMovement
     }
     private void ExplodeEnemy()
     {
-        Debug.Log("no");
+        DealDamage();
+        Instantiate(ParticleSystem, transform.position, Quaternion.identity);
+        Instantiate(flame, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+    public void DealDamage()
+    {
+        Collider2D hitCollider = Physics2D.OverlapCircle(transform.position, damageRadius, targetLayer);
+        if (hitCollider == null) return;
+        PlayerStats character = hitCollider.GetComponent<PlayerStats>();
+        if (character != null) character.TakeDamage(damageAmount);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red; // Viền đỏ rõ hơn
+        Gizmos.DrawWireSphere(transform.position, damageRadius);
     }
 }
