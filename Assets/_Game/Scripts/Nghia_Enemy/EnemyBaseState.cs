5ab9ed055c698b7c5fb06ba11fda755749d36c96
dt74aa <130040319+dt74aa@users.noreply.github.com>
2025-02-14 18:28:40 +0700

    nghia

---
+++ Assets/_Game/Scripts/Nghia_Enemy/EnemyBaseState.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public abstract class EnemyBaseState
+{
+    public abstract void EnterState(EnemyStateMachine enemy);
+    public abstract void UpdateState(EnemyStateMachine enemy);
+    public abstract void ExitState(EnemyStateMachine enemy);
+}
