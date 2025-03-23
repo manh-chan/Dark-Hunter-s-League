a0af486cc5ea9d1cd901bea2cc0ef626483c9e54
tatuongbachhh <149562415+tatuongbachhh@users.noreply.github.com>
2025-03-23 15:05:10 +0700

    bach 8.3

---
+++ Assets/CanvasMap.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasMap : UICanvas
+{
+    public void PlayButton()
+    {
+        Close(0);
+        UIManager.Instance.OpenUI<CanvasGamePlay>();
+    }
+}
