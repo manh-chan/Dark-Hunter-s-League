905ef855bb01335fea94c0261526a8365e384313
manh-chan <hoangbuimyt@gmail.com>
2025-04-04 20:59:44 +0700

    manh

--- Assets/_Game/Scripts/UI/CanvasHero.cs
+++ Assets/_Game/Scripts/UI/CanvasHero.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static Cinemachine.DocumentationSortingAttribute;
 
 public class CanvasHero : UICanvas
 {
